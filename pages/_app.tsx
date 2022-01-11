4830f50afe633bb910567a29e8b1218db7cd4fbe
Austin <austin@r00ks.io>
2022-01-11 14:57:10 -0600

    move utility import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/tailwind.css'
+import 'tailwindcss/utilities.css'
 import '../styles/toggle.css'
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,43 +6,32 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
-            <script 
-              key='r00ks-theme'
-              dangerouslySetInnerHTML={{ 
-                __html: `
-                (function() {
-                  window.__onThemeChange = function() {};
-                  function setTheme(newTheme) {
-                    window.__theme = newTheme;
-                    preferredTheme = newTheme;
-                    document.body.className = newTheme;
-                    if (newTheme == "dark") {
+          <script 
+            key='r00ks-theme'
+            dangerouslySetInnerHTML={{ 
+              __html: `
+              (function() {
+                  var theme = localStorage.getItem('theme');
+                  var supportDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches === true;
+                  if (!theme && supportDarkMode) {
+                      document.body.classList.add('dark');
                       document.querySelector('meta[name="theme-color"]').setAttribute("content", '#0e1117');
-                    } else {
+                  }
+                  if (!theme) {
                       document.querySelector('meta[name="theme-color"]').setAttribute("content", '#3164dc');
-                    }
-                    window.__onThemeChange(newTheme);
+                      return;
                   }
-                  var preferredTheme;
-                  try {
-                    preferredTheme = localStorage.getItem('theme');
-                  } catch (err) { }
-                  window.__setPreferredTheme = function(newTheme) {
-                    setTheme(newTheme);
-                    try {
-                      localStorage.setItem('theme', newTheme);
-                    } catch (err) {}
-                  }
-                  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
-                  darkQuery.addListener(function(e) {
-                    window.__setPreferredTheme(e.matches ? 'dark' : 'light')
-                  });
-                  setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
-                })();`,
-              }}
-            />
-            <Main />
-            <NextScript />
+                  document.body.classList.add(theme);
+                  if (theme === 'dark') {
+                      document.querySelector('meta[name="theme-color"]').setAttribute("content", '#0e1117');
+                  } else {
+                      document.querySelector('meta[name="theme-color"]').setAttribute("content", '#3164dc');
+                  } 
+              })();`,
+            }}
+          />
+          <Main />
+          <NextScript />
         </body>
       </Html>
     )
