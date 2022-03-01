ae0b7bc5565863ae6f0665d1d097ed40772badf0
Austin <austin@r00ks.io>
2022-03-01 10:23:51 -0600

    fix favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,24 +8,24 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/favicons/apple-touch-icon.png"
+            href="/public/favicons/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicons/favicon-32x32.png"
+            href="/public/favicons/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicons/favicon-16x16.png"
+            href="/public/favicons/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
           <link
             rel="mask-icon"
-            href="/favicons/safari-pinned-tab.svg"
+            href="/public/favicons/safari-pinned-tab.svg"
             color="#5bbad5"
           />
           <meta name="msapplication-TileColor" content="#000000" />
